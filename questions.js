7e27d47f77340a7d75c8c659429e2b801505ddb6
Caillou <Boucher.Pierre@hotmail.fr>
2018-10-11 09:19:24 +0200

    Partie faite

---
+++ questions.js
@@ -0,0 +1,55 @@
+var languagesArrayCreation = function () {
+ var languages = ['Html', 'CSS', 'Java', 'PHP'];
+  return languages;
+}
+
+var numbersArrayCreation = function () {
+  var numbers = [0, 1, 2, 3, 4, 5];
+    return numbers;
+}
+
+var ElementReplacement = function (languages) {
+  languages[2]='Javascript';
+  return languages;
+}
+
+var AddElementToLanguagesArray = function (languages) {
+  languages.push('Ruby', 'Python');
+  return languages;
+}
+
+var AddElementToNumbersArray = function (numbers) {
+  numbers.unshift(-2,-1);
+  return numbers;
+}
+
+var deleteArrayFirstElement = function (languages) {
+  languages.shift();
+  return languages;
+}
+
+var deleteArrayLastElement = function (languages) {
+  languages.pop();
+  return languages;
+}
+
+var stringToArray = function (socialMediaInString) {
+
+  return socialMediaInString.split(',');
+}
+
+var arrayToString = function (languages) {
+  Temp = languages.join(',');
+  return Temp;
+}
+
+var arraySort = function (socialMedia) {
+  socialMedia.sort();
+  return socialMedia;
+}
+
+var arrayInvert = function (languages){
+  languages.reverse();
+
+  return languages;
+}
